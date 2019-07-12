b38ed843db46d5335a12ede221992277e218b974
Ray Xu <xurj@pku.edu.cn>
2019-07-12 16:04:44 +0800

    Update fastai_toolbox.py

--- fastai_toolbox.py
+++ fastai_toolbox.py
@@ -3,18 +3,22 @@ def find_proper_lr(learner):
     """
     learner.lr_find()
     learner.recorder.plot(suggestion=True)
-
+    
+    
 def show_all(learner, print=print):
-    """VERIFIED by kaggle
+    """VERIFIED by kaggle, result dropped, why? may be deviation
     显示learner在训练后的所有情况
     """
-    learner.plot_metrics()
-    print('<---Metrics--->')
-    print(learner.recorder.metrics)
+    if len(learner.metrics) > 0:
+        learner.recorder.plot_metrics()
+        print('<---Metrics--->')
+        print(learner.recorder.metrics)
+    else:
+        print('<---NO Metrics--->')
     
     learner.recorder.plot_losses()
-    print('<---Train Losses--->')
-    print(learner.recorder.train_losses)
+#     print('<---Train Losses--->')
+#     print(learner.recorder.losses)
     print('<---Val Losses--->')
     print(learner.recorder.val_losses)
     
