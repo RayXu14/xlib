7dfb12c88ac6a1e4dde2d3faf7f6f2d8d1f39439
root <xurj@pku.edu.cn>
2019-12-16 04:01:51 +0000

    expand dataset.py with common operation

--- dataset.py
+++ dataset.py
@@ -1,5 +1,7 @@
 import random
 import pickle as pkl
+from collections import Counter
+import sys
 
 from tqdm import tqdm
 
@@ -30,7 +32,7 @@ def calc_avg_tokens(*args):
     for arg in args:
         if type(arg) = str:  # path
             with open_utf8(path) as f:
-                for utt in tqdm(zip(fs, ft)):
+                for utt in tqdm(zip(fs, ft), desc='Calculating average tokens'):
                     words = utt.strip().split()
                     lens.append(len(words))
         elif type(arg) = list:
@@ -51,28 +53,11 @@ def load_pkl(path):
     with open(path, 'rb') as f:
         content = pkl.load(f)
     print(f'Loaded from {path}')
-
-
-def overview_list(lst, name='list', n=3):
-    print(f'{name} has {len(lst)} elements')
-    if n > 0:
-        print(f'Head {n} examples')
-        print(lst[:n])
-        print(f'Tail {n} examples')
-        print(lst[-n:])
         
 
 def dump_pkl(path, content):
     with open(path, 'wb') as f:
         pkl.dump(content, f)
-        
-
-def uttListFile2wordsListFile(origin_path: str, method, output_path=None):
-    output_path = origin_path + '.tok' if output_path is None else output_path
-    with open_utf8(origin_path) as f, open_utf8(output_path, 'w') as ftok:
-        for line in tqdm(f):
-            words = method(line)
-            ftok.write(' '.join(words) + '\n')
         
 
 def uttList2wordsList(utts: list, method):
@@ -104,3 +89,66 @@ def spacy_cut(utt: str):
     utt = utt.strip()
     words = [w.text for w in nlp(utt.strip())]
     return words
+        
+
+def uttListFile2wordsListFile(origin_path: str, method, output_path=None):
+    output_path = origin_path + '.tok' if output_path is None else output_path
+    with open_utf8(origin_path) as f, open_utf8(output_path, 'w') as ftok:
+        for line in tqdm(f, desc=f'Tokenizing {origin_path}'):
+            words = method(line)
+            ftok.write(' '.join(words) + '\n')
+
+
+def overview_list(lst, name='list', n=3):
+    print(f'{name} has {len(lst)} elements')
+    if n > 0:
+        print(f'Head {n} examples')
+        print(lst[:n])
+        print(f'Tail {n} examples')
+        print(lst[-n:])
+        
+
+def init_simple_vocab(include_stctag=True):
+    counter = Counter()
+    if include_stctag:
+        counter['<bos>'] = sys.maxsize
+        counter['<eos>'] = sys.maxsize
+    counter['<pad>'] = sys.maxsize
+    counter['<unk>'] = sys.maxsize
+    return counter
+
+
+def load_wordsListFile(path, counter=None):
+    if counter is not None:
+        origin_cnt_size = len(counter)
+    
+    wordsList = []
+    with open_utf8(path) as f:
+        for line in tqdm(f, desc=f'Loading {path}'):
+            words = line.strip().split()
+            if counter is not None:
+                counter.update(words)
+            wordsList.append(words)
+            
+    overview_list(wordsList, name='Loaded set')
+    
+    if counter is None:
+        return wordsList
+    else:
+        print(f'Word Counter size: {len(counter)}')
+        print('10 most common words:')
+        print(counter.most_common(10))
+        return wordsList, counter
+    
+
+def counter2vocab(counter, max_vocab=None):
+    max_vocab = len(counter) if max_vocab is None else max_vocab
+    print(f'Final Vocab size: {max_vocab}/{len(counter)}')
+
+    vocab = []
+    for word, cnt in counter.most_common(max_vocab):
+        vocab.append(word)
+    return vocab
+    
+    
+    
