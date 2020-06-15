8ae950ce4e6b59efe586f7a1dcc9a213137239d5
Ray Xu <xurj@pku.edu.cn>
2020-06-15 21:17:22 +0800

    Delete bokeh_toolbox.py

--- bokeh_toolbox.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def lon8lat_to_mercator(lon, lat):
-
-    k = 6378137.
-    x = lon * (k * math.pi/180.)
-    y = math.log(math.tan((90. + lat) * math.pi/360.)) * k
-
-    return x, y
